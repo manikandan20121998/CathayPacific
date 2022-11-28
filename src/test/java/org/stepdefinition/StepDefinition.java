84eec6d3e7629134fb939147af3f50323db87691
manikandan20121998 <manimaddy978@gmail.com>
2022-11-28 21:31:33 +0530

    mani code of stepdefinition

--- src/test/java/org/stepdefinition/StepDefinition.java
+++ src/test/java/org/stepdefinition/StepDefinition.java
@@ -1,5 +1,44 @@
 package org.stepdefinition;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class StepDefinition {
+	@Given("user has to in verified website")
+	public void user_has_to_in_verified_website() {
+
+	}
+
+	@When("user give the from place of take")
+	public void user_give_the_from_place_of_take() {
+
+	}
+
+	@When("user give the to place for their destination")
+	public void user_give_the_to_place_for_their_destination() {
+
+	}
+
+	@When("user has to give date of travel")
+	public void user_has_to_give_date_of_travel() {
+
+	}
+
+	@When("user has to give date of return")
+	public void user_has_to_give_date_of_return() {
+
+	}
+
+	@When("user has to click search button for check available fight lists")
+	public void user_has_to_click_search_button_for_check_available_fight_lists() {
+
+	}
+
+	@Then("user see the fight list")
+	public void user_see_the_fight_list() {
+
+	}
+
 
 }
